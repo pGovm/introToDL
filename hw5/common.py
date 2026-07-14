c64d8860fd55b3a708cb423cafbc06b1caa8f55f
pGovm <pgovindmenon07@gmail.com>
2026-07-14 19:55:04 -0400

    Added the tqdm progress bar

--- hw5/common.py
+++ hw5/common.py
@@ -10,6 +10,7 @@ import torchvision
 import torchvision.transforms as transforms
 
 from torchinfo import summary
+from tqdm import tqdm
 
 # Initializing the device we will be using
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -63,6 +64,10 @@ def evaluate(model, test_loader, device):
 def train_model(model, train_loader, test_loader, lr, epochs):
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=lr)
+    
+    # Adding scheduling to improve test acc
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer=optimizer, T_max=epochs)
+
     epoch_time = 0.0
 
     for epoch in range(epochs):
@@ -72,7 +77,9 @@ def train_model(model, train_loader, test_loader, lr, epochs):
         model.train()
         train_loss = 0.0
 
-        for images, labels in train_loader:
+        progress_bar = tqdm(train_loader, desc=f'Epoch ({epoch+1}/{epochs})')
+
+        for images, labels in progress_bar:
             images = images.to(device)
             labels = labels.to(device)
 
@@ -82,9 +89,14 @@ def train_model(model, train_loader, test_loader, lr, epochs):
             loss = criterion(logits, labels)
             optimizer.zero_grad()
             loss.backward()
+
+            # Adding clipping to try to improve test accuracy
+            nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
             optimizer.step()
 
             train_loss += loss.item()
+        
+        scheduler.step()
 
         avg_loss = train_loss / len(train_loader)
         train_time = time.time() - start
